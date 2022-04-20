adbd883ae963b5363172b023354ece894f6d4f2f
Даниил Федин <nniiyu@yandex.ru>
2022-04-20 18:39:31 +0300

    update

--- Day3.js
+++ Day3.js
@@ -1,12 +1,12 @@
-name='Den'
-city='Saransk'
+const name='Den'
+const city='Saransk'
 console.log(`Hey,my name is ${name}.I live in the city ${city}`)
 
 const multByFactor1=function(value,multiplier){
     if(multiplier!=undefined){
         return value*multiplier
     }
-    return value
+    return value;
 }
 const multByFactor2=(value,multiplier)=>{
     if(multiplier!=undefined){
@@ -34,6 +34,51 @@ const post={
     id:0,
     author:'Den'
 }
-console.log(newPost1(post))
-console.log(newPost2(post))
+console.log(newPost1(post));
+console.log(newPost2(post));
+
+const newError=()=>{
+    throw new Error('Death');
+}
+try{
+    newError();
+}catch(error){
+    console.log(error.message);
+}
+console.log('Continue...');
+
+const myArray1 = []
+console.log(myArray1)
+myArray1[1]=1
+console.log(myArray1)
+console.log(myArray1[0])
+console.log(myArray1[2])
+console.log(myArray1.shift())
+console.log(myArray1)
+console.log(myArray1.length=2)
+console.log(myArray1)
+myArray1[1]=null
+myArray1.forEach((elem)=>{console.log(2)})
+console.log(myArray1)
+const newArray1=myArray1.map(function(elem){return 3})
+console.log(newArray1)
+
+const person={
+    nickname:'Den',
+    age:20,
+    specialization:'FrontEnd Dev'
+}
+if(true){
+    const {nickname,age}=person
+    console.log(nickname)
+    console.log(age)
+}
+const newPerson=({nickname,age})=>{
+    return {
+        nickname,
+        age:++age
+    }
+}
+console.log(newPerson(person))
+
 

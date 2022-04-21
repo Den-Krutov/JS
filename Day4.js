f4bf287b1079bee5f128fc896acac96f76a69974
Даниил Федин <nniiyu@yandex.ru>
2022-04-21 18:07:10 +0300

    update

--- Day4.js
+++ Day4.js
@@ -38,3 +38,48 @@ for(const symbol of nickname){
     console.log(symbol)
 }
 
+class Comment{
+    constructor(text){
+        this.text=text
+        this.votesQty=0
+    }
+    upvote(){
+        this.votesQty+=1
+    }
+    static mergeComments(text1,text2){
+        console.log(text1,text2)
+    }
+}
+
+const firstComment = new Comment('First comment')
+console.log(firstComment)
+firstComment.upvote()
+console.log(firstComment)
+const secondComment = new Comment('Second comment')
+console.log(secondComment)
+secondComment.upvote()
+console.log(secondComment)
+console.log(firstComment)
+Comment.mergeComments('Hey,'+'i am Den')
+
+class Calc extends Array{
+    sum(){
+        return this.reduce((elem,acc)=>acc+=elem,0)
+    }
+    multiplies(){
+        return this.reduce((elem,acc)=>acc*=elem,1)
+    }
+}
+const numberCalc = new Calc(1,3,2)
+console.log(numberCalc.sum())
+console.log(numberCalc.multiplies())
+
+const arrNumbers=[1,3,2,4]
+
+const rez=arrNumbers.reduce((elem,acc)=>acc+=elem)
+console.log(rez)
+
+console.log(nickname.length)
+const nickname2=new String('Den Krutov')
+console.dir(nickname2)
+
